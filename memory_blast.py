a26bbda38286a49da02b4f23837dcf28e3b1ccfa
Asare L. Cobblah <cobblaha@gmail.com>
2017-04-20 13:46:18 -0500

    Improved intent recognition with custom slot type. Added game functionality.

--- memory_blast.py
+++ memory_blast.py
@@ -18,60 +18,38 @@ logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 def new_game():
 
     welcome_msg = render_template('welcome')
+    welcome_rempromt_msg = render_template('welcome_repromt')
 
     return question(welcome_msg)
 
 
-@ask.intent("YesIntent")
-def sel_level():
-    sel_lvl_msg = render_template('sel_level')
-    
-    return question(sel_lvl_msg)
-
-
 @ask.intent("LevelIntent", convert={'lvl': int})
-def next_round(lvl):
-    max_level = 10
-    if lvl >= 10 :
+def first_round(lvl):
+    if (lvl > 10) or (lvl < 1):
         
-        sel_lvl_msg = render_template('sel_level_repromt_h')
-        return question(sel_lvl_msg)
-        
-    elif lvl < 2:
-        
-        sel_lvl_msg = render_template('sel_level_repromt_l')
+        sel_lvl_msg = render_template('welcome_repromt')
         return question(sel_lvl_msg)
     
     else:
         
-        session.attributes['level'] = lvl   #store level
-    
-        numbers = [randint(0, 9) for _ in range(lvl)]
-
+        numbers = [randint(0, 9) for _ in range(lvl+1)]
         
-        lvl_indicator = render_template('lvl_ind', lvl=lvl)
-        round_msg = render_template('round', numbers=numbers)
+        #store session variables
+        session.attributes['level'] = lvl   
+        session.attributes['numbers'] = numbers 
 
-        session.attributes['numbers'] = numbers
+        round_msg = render_template('round', lvl=lvl, numbers=numbers)
 
-        round_msg = lvl_indicator + round_msg
         return question(round_msg)
 
 
-@ask.intent("NoIntent")
-def exit_app():
-    bye_msg = render_template('goodbye')
-    
-    return statement(bye_msg)
-
-
-@ask.intent("AnswerIntent", convert={'first': int, 'second': int, 'third': int, 'fourth': int, 'fifth': int, 'sixth': int, 'seventh': int, 'eighth': int, 'ninth': int, 'tenth': int})
-def answer(first, second, third, fourth, fifth, sixth, seventh, eighth, ninth, tenth):
+@ask.intent("AnswerIntent", convert={'first': int, 'second': int, 'third': int, 'fourth': int, 'fifth': int, 'sixth': int, 'seventh': int, 'eighth': int, 'ninth': int, 'tenth': int, 'eleventh': int})
+def answer(first, second, third, fourth, fifth, sixth, seventh, eighth, ninth, tenth, eleventh):
 
     winning_numbers = session.attributes['numbers']
     
-    usr_ans = [first, second, third, fourth, fifth, sixth, seventh, eighth, ninth, tenth]
-    usr_ans = usr_ans[0:(session.attributes['level'])]
+    usr_ans = [first, second, third, fourth, fifth, sixth, seventh, eighth, ninth, tenth, eleventh]
+    usr_ans = usr_ans[0:(session.attributes['level']+1)]
     session.attributes['usr_ans'] = usr_ans
     
 
@@ -85,12 +63,45 @@ def answer(first, second, third, fourth, fifth, sixth, seventh, eighth, ninth, t
         msg = render_template('lose')
         lvl_down(session.attributes['level'])
 
-    #do next round    
-    next_round(session.attributes['level']);   
-    
-    return question(msg)
+
+    return next_round(msg, session.attributes['level'])
+
+@ask.intent('AMAZON.HelpIntent')
+def help():
+    help_text = render_template('help')
+    return question(help_text)
 
 
+@ask.intent('AMAZON.StopIntent')
+def stop():
+    bye_text = render_template('goodbye')
+    return statement(bye_text)
+
+
+@ask.intent('AMAZON.CancelIntent')
+def cancel():
+    bye_text = render_template('goodbye')
+    return statement(bye_text)
+
+
+@ask.session_ended
+def session_ended():
+    return "{}", 200
+
+
+def next_round(msg, lvl):
+    numbers = [randint(0, 9) for _ in range(lvl+1)]
+
+    #store session variables
+    session.attributes['level'] = lvl   
+    session.attributes['numbers'] = numbers 
+
+    round_msg = msg + render_template('round', lvl=lvl, numbers=numbers)
+
+    return question(round_msg)
+
+
+#helper functions
 def lvl_up(lvl):
     if lvl < 10:
         session.attributes['level'] += 1
